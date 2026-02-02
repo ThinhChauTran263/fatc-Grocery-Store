7bd7943d126569cceed7bbcb2ee16a6de04e9771
ChenZhouThinh2636 <thinhtran2632006@gmail.com>
2026-02-03 01:58:01 +0700

    Add JVM timezone init in main application

--- src/main/java/poly/edu/java5_asm/Java5AsmApplication.java
+++ src/main/java/poly/edu/java5_asm/Java5AsmApplication.java
@@ -5,10 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
 public class Java5AsmApplication {
+
+    @PostConstruct
+    public void init() {
+        // Set default timezone to Vietnam
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Java5AsmApplication.class, args);
